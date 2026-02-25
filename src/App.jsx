cfa628ac43106920b1d0b9e869a705dd620aa38e
Archit Verma <architv2023@gmail.com>
2026-02-26 01:33:04 +0530

    React folder completed

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <div class="div1">
+        <h1>Hello world</h1>
+        <h2>My name is archit</h2>
+      </div>
+
+      <div class="div2">
+        <h1>Achooo</h1>
+        <h2>How do you do</h2>
+      </div>
+
+    </> 
+  )
+}
+
+export default App
